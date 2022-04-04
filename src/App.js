f6c6358686cfcb62c9d49adb771301c1d43456e0
ZeroBug <404.zerobug@gmail.com>
2022-04-04 14:46:59 +0600

    router setup done with no reload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import Helmet from "react-helmet";
+import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import Blog from "./component/Blog/Blog";
+import Header from "./component/Header/Header";
+import Home from "./component/Home/Home";
+import NotFound from "./component/NotFound/NotFound";
+import Reviews from "./component/Reviews/Reviews";
 
 function App() {
   return (
     <div className="App">
-      
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Laptop Review</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+
+      <Header></Header>
+
+      <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+          <Route path="/blog" element={<Blog></Blog>}></Route>
+          <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
